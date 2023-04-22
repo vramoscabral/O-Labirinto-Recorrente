2a04592154919e1af7f503be4c0c12370936d164
vramoscabral <127407951+vramoscabral@users.noreply.github.com>
2023-04-22 11:41:58 -0300

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "mat.hpp"
+using namespace std;
+
+int main() {
+  Matriz m;
+
+  m.Readfile();
+  m.OpenMatrix();
+  m.Output();
+  m.remove();
+  
+  cout << "\n---- RESULTADO FINAL DO JOGO----" << endl;
+  if (m.sabc()==1)
+    cout << "Encerrado pois o total de vidas chegou a 0\n" << endl;
+  else
+  {
+    cout << "Encerrado pois nao tinha mais possibilidade de pegar itens" << endl;
+    cout << "Restavam " << m.seevida() << " vida(s)\n" << endl;
+  }
+  cout << "Total de casas percorridas: " << m.seecasas() << endl;
+  cout << "Total de itens consumidos: " << m.seeitens() << endl;
+  cout << "Casas que não foram exploradas: " << m.NotVisited() << endl;
+  cout << "Total de perigos enfrentados: " << m.seeperigo() << endl;
+  
+  return 0;
+}
---
+++ src/mat.hpp
@@ -0,0 +1,45 @@
+#ifndef MAT_HPP
+#define MAT_HPP
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <string>
+#include <cstdio>
+using namespace std;
+
+class Matriz
+{
+  private:
+    int nummat;
+    int tammat;
+    int start[2];
+    static int casas;
+    static int vida;
+    static int vida2;
+    static int itens2;
+    static int itens;
+    static int perigo;
+    static int ntimes;
+    static int abc;
+    
+  public:
+    Matriz();
+
+    int seecasas();
+    int seeperigo();
+    int seeitens();
+    int seevida();
+    int sabc();
+    void Readfile();
+    int Tam();
+    int Quant();
+    void OpenMatrix();
+    int WalkMatrix(int **mtrx, int im, int jm, int rsize, int n);
+    int NotVisited();
+    void Output();
+    void remove();
+    /*void ReadMatrix();
+    void WalkMatrix(int **m, int size);
+    void SumUpTimes();*/
+};
+#endif
